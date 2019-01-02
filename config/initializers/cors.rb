410f99a184136fed9678689dfea4c758150ad4d4
Kevin Chappell <kevin.b.chappell@gmail.com>
2019-01-02 21:52:26 +0000

    make origins an ENV var with fallback to frontend staging url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # Add middleware to handle CORS since frontend and backend have different domains
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://petuum.herokuapp.com'
+    origins ENV.fetch('FRONTEND_DOMAIN') { 'https://petuum.herokuapp.com' }
     resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
 end
