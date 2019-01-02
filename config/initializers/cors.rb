526a1f299d3534273b2f460ae144c1e0654a07bb
Kevin Chappell <kevin.b.chappell@gmail.com>
2019-01-02 21:46:12 +0000

    add comment explaining middleware purpose

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,3 +1,4 @@
+# Add middleware to handle CORS since frontend and backend have different domains
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000', 'https://petuum.herokuapp.com'
