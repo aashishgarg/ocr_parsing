7949a3083d37ae3032c1b9b8e5196546a9fb7bca
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-09 23:17:28 +0530

    User_controller tests updated

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -13,13 +13,13 @@ RSpec.describe Api::UsersController, type: :controller do
   context '#update' do
     it 'returns http Unauthorized' do
       request.headers.merge!('Authorization' => "Bearer #{@token}111111")
-      get :update, params: {user: {name: 'TestName'}}, format: 'json'
+      put :update, params: {user: {name: 'TestName'}}, format: 'json'
       expect(response.status).to eq 401
     end
 
     it 'returns http Forbidden for user with no role' do
       request.headers.merge!('Authorization' => "Bearer #{@token}")
-      get :update, params: {user: {name: 'TestName'}}, format: 'json'
+      put :update, params: {user: {name: 'TestName'}}, format: 'json'
       expect(response.status).to eq 403
     end
 
@@ -27,14 +27,37 @@ RSpec.describe Api::UsersController, type: :controller do
       @user.add_role :customer
       @user.add_role :support
       request.headers.merge!('Authorization' => "Bearer #{@token}")
-      get :update, params: {user: {name: 'TestName'}}, format: 'json'
+      put :update, params: {user: {name: 'TestName'}}, format: 'json'
       expect(response.status).to eq 403
+      expect(response.content_type).to eq 'text/html'
+      expect(response.body).to eq ''
     end
 
     it 'returns http success' do
       @user.add_role :admin
-      get :update, params: {user: {name: 'TestName'}}, format: 'json'
+      put :update, params: {user: {name: 'TestName'}}, format: 'json'
       expect(response.status).to eq 200
+      expect(response.content_type).to eq 'application/json'
+    end
+  end
+
+  context '#show' do
+    it 'returns http Unauthorized' do
+      request.headers.merge!('Authorization' => "Bearer #{@token}111111")
+      get :show, format: 'json'
+      expect(response.status).to eq 401
+    end
+
+    it 'returns http success' do
+      get :show, format: 'json'
+      expect(response.status).to eq 200
+    end
+
+    it 'returns user details' do
+      get :show, format: 'json'
+      hash_body = JSON.parse(response.body).with_indifferent_access
+      expect(hash_body.keys.include?('user')).to eq(true)
+      %w(id email first_name last_name company phone fax token).each { |key| expect(hash_body['user'].keys.include?(key)).to eq(true) }
     end
   end
 end
