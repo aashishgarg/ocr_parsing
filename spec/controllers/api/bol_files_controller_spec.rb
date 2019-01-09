5e085100298c3d0a5382af9134568c0c66fc17ab
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-09 19:16:44 +0530

    bol_type_controller_spec updated

---
+++ spec/controllers/api/bol_files_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+require 'jwt'
+
+RSpec.describe Api::BolFilesController, type: :controller do
+
+end
--- spec/controllers/api/bol_types_controller_spec.rb
+++ spec/controllers/api/bol_types_controller_spec.rb
@@ -26,21 +26,39 @@ RSpec.describe Api::BolTypesController, type: :controller do
       hash_body = JSON.parse(response.body).with_indifferent_access
       expect(hash_body).to match({bol_types: []})
     end
+
+    it 'returns non empty array of BOL Types' do
+      bol_types = []
+      2.times { bol_types << FactoryBot.create(:bol_type) }
+      get :index, format: 'json'
+      hash_body = JSON.parse(response.body).with_indifferent_access
+      expect(hash_body).to match({bol_types: [{id: bol_types[0].id, name: bol_types[0].name},
+                                              {id: bol_types[1].id, name: bol_types[1].name}]
+                                 })
+    end
   end
 
   context '#show' do
     before do
       @bol_type = FactoryBot.create(:bol_type)
     end
-    it 'returns http success' do
-      get :show, params: {id: @bol_type.id}, format: 'json'
-      expect(response.status).to eq 200
-    end
 
     it 'returns http Unauthorized' do
       request.headers.merge!('Authorization' => "Bearer #{@token}111111")
       get :show, params: {id: @bol_type.id}, format: 'application/json'
       expect(response.status).to eq 401
+    end
+
+    it 'returns http success' do
+      get :show, params: {id: @bol_type.id}, format: 'json'
+      expect(response.status).to eq 200
+    end
+
+    it 'returns BOL Type hash' do
+      get :show, params: {id: @bol_type.id}, format: 'application/json'
+      hash_body = JSON.parse(response.body).with_indifferent_access
+      expect(hash_body.keys.include?('bol_type')).to eq(true)
+      %w(id name).each {|key| expect(hash_body['bol_type'].keys.include?(key)).to eq(true)}
     end
   end
 end
