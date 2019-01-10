187c7c3ac980dc1410d5bc99fee6b2ef30149785
rautelaanirudh <anirudhsinghrautela@gmail.com>
2019-01-10 16:33:48 +0530

    Remove unwanted folders Fix folder creation for processing files

--- app/services/s3/process_files.rb
+++ app/services/s3/process_files.rb
@@ -8,18 +8,10 @@ module S3
     def process_s3_request
       file_urls = bol_file.get_attachment_urls
       @urls.each {|url| download_s3_file(url)}
-
-      # header = {"Content-type": "application/json"}
-      # response = open(bol_file_object) {|f| f.read }
-      # base64_encoded_image = Base64.encode64(response)
-      # http = Net::HTTP.new(@uri.host, @uri.port)
-      # request = Net::HTTP::Post.new(@uri.request_uri, header)
-      # request.body = base64_encoded_image
-      # response = http.request(request)
     end
 
     def download_s3_file
-      directory_name = "#{Rails.root}/public/bol_files/original"
+      directory_name = "#{Rails.root}/public/bol_files"
       Dir.mkdir(directory_name) unless Dir.exists?(directory_name)
       download(directory_name, @key)
     end
