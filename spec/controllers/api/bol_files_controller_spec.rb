c1cccc86cb003e29270b87758bdbf56fef513334
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-10 13:09:21 +0530

    tests for bol_file authorization for role based - Updated

--- spec/controllers/api/bol_files_controller_spec.rb
+++ spec/controllers/api/bol_files_controller_spec.rb
@@ -26,6 +26,7 @@ RSpec.describe Api::BolFilesController, type: :controller do
 
   context '#show' do
     before do
+      User.current = @user
       @bol_file = FactoryBot.create(:bol_file)
     end
 
@@ -41,12 +42,69 @@ RSpec.describe Api::BolFilesController, type: :controller do
     end
   end
 
+  context '#create' do
+    before do
+      User.current = @user
+      @bol_type = FactoryBot.create(:bol_type)
+    end
+
+    it 'not allowed for user role :customer' do
+      User.current.roles.delete(Role.where(name: [:admin, :support]))
+      User.current.add_role :customer
+      post :create, params: {shipper_id: @shipper.id, bol_file: {
+                      name: 'NewTestName',
+                      Shipper_id: @shipper.id,
+                      bol_type_id: @bol_type.id
+                  }}, format: 'json'
+      expect(response.status).to eq 403
+    end
+
+    it 'allowed for user role :support' do
+      User.current.roles.delete(Role.where(name: [:admin, :customer]))
+      User.current.add_role :support
+      post :create, params: {shipper_id: @shipper.id, bol_file: {
+                      name: 'NewTestName',
+                      Shipper_id: @shipper.id,
+                      bol_type_id: @bol_type.id
+                  }}, format: 'json'
+      expect(response.status).to eq 200
+    end
+
+    it 'allowed for user role :admin' do
+      User.current.roles.delete(Role.where(name: [:customer, :support]))
+      User.current.add_role :admin
+      post :create, params: {shipper_id: @shipper.id, bol_file: {
+                      name: 'NewTestName',
+                      Shipper_id: @shipper.id,
+                      bol_type_id: @bol_type.id
+                  }}, format: 'json'
+      expect(response.status).to eq 200
+    end
+  end
+
   context '#update' do
     before do
+      User.current = @user
       @bol_file = FactoryBot.create(:bol_file)
     end
 
-    it 'allowed for user role :customer and :support' do
+    it 'allowed for user role :customer' do
+      User.current.roles.delete(Role.where(name: [:admin, :support]))
+      User.current.add_role :customer
+      put :update, params: {shipper_id: @shipper.id, id: @bol_file.id, bol_file: {name: 'NewTestName'}}, format: 'json'
+      expect(response.status).to eq 200
+    end
+
+    it 'allowed for user role :support' do
+      User.current.roles.delete(Role.where(name: [:admin, :customer]))
+      User.current.add_role :support
+      put :update, params: {shipper_id: @shipper.id, id: @bol_file.id, bol_file: {name: 'NewTestName'}}, format: 'json'
+      expect(response.status).to eq 200
+    end
+
+    it 'allowed for user role :admin' do
+      User.current.roles.delete(Role.where(name: [:customer, :support]))
+      User.current.add_role :admin
       put :update, params: {shipper_id: @shipper.id, id: @bol_file.id, bol_file: {name: 'NewTestName'}}, format: 'json'
       expect(response.status).to eq 200
     end
