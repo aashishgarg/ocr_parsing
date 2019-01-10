6912966bee360f19167120399aad5c1206bcade7
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-10 16:48:42 +0530

    Models- (bol_type, shipper) removed

--- app/models/bol_type.rb
+++
@@ -1,6 +0,0 @@
-class BolType < ApplicationRecord
-  has_many :bol_files
-
-  validates :name, uniqueness: true, presence: true, allow_blank: false # Add scope if shipper_id is needed
-  # belongs_to :shipper
-end
