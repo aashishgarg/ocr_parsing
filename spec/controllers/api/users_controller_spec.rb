d825d3dc0ac3cb6edbecc6dcfef0568aa2e5d46e
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-11 12:06:33 +0530

    Rubocop suggestions implemented in users_controller_specs

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -4,38 +4,40 @@ require 'jwt'
 RSpec.describe Api::UsersController, type: :controller do
   before do
     @user = FactoryBot.create(:user)
-    @token = JWT.encode({id: @user.id}, ENV['DEVISE_JWT_SECRET_KEY'])
-    request.headers.merge!('Authorization' => "Bearer #{@token}")
-    request.headers.merge!('content-type' => 'application/json')
-    request.headers.merge!('Accept' => 'application/json')
+    @user.add_role :admin
+    @token = JWT.encode({ id: @user.id }, ENV['DEVISE_JWT_SECRET_KEY'])
+    request.headers['Authorization'] = "Bearer #{@token}"
+    request.headers['content-type'] = 'application/json'
+    request.headers['Accept'] = 'application/json'
   end
 
   context '#update' do
     it 'returns http Unauthorized' do
-      request.headers.merge!('Authorization' => "Bearer #{@token}111111")
-      put :update, params: {user: {name: 'TestName'}}, format: 'json'
+      request.headers['Authorization'] = "Bearer #{@token}111111"
+      put :update, params: { user: { name: 'TestName' } }, format: 'json'
       expect(response.status).to eq 401
     end
 
     it 'returns http Forbidden for user with no role' do
-      request.headers.merge!('Authorization' => "Bearer #{@token}")
-      put :update, params: {user: {name: 'TestName'}}, format: 'json'
+      @user.roles.delete(Role.all)
+      @user.roles.each { |role| @user.roles.delete(role) }
+      put :update, params: { user: { name: 'TestName' } }, format: 'json'
       expect(response.status).to eq 403
     end
 
     it 'returns http Forbidden for user with non admin role' do
+      @user.roles.delete(Role.find_by(name: 'admin'))
       @user.add_role :customer
       @user.add_role :support
-      request.headers.merge!('Authorization' => "Bearer #{@token}")
-      put :update, params: {user: {name: 'TestName'}}, format: 'json'
+      put :update, params: { user: { name: 'TestName' } }, format: 'json'
       expect(response.status).to eq 403
-      expect(response.content_type).to eq 'text/html'
+      expect(response.content_type).to eq 'application/json'
       expect(response.body).to eq ''
     end
 
     it 'returns http success' do
       @user.add_role :admin
-      put :update, params: {user: {name: 'TestName'}}, format: 'json'
+      put :update, params: { user: { name: 'TestName' } }, format: 'json'
       expect(response.status).to eq 200
       expect(response.content_type).to eq 'application/json'
     end
@@ -43,7 +45,7 @@ RSpec.describe Api::UsersController, type: :controller do
 
   context '#show' do
     it 'returns http Unauthorized' do
-      request.headers.merge!('Authorization' => "Bearer #{@token}111111")
+      request.headers['Authorization'] = "Bearer #{@token}111111"
       get :show, format: 'json'
       expect(response.status).to eq 401
     end
@@ -56,8 +58,10 @@ RSpec.describe Api::UsersController, type: :controller do
     it 'returns user details' do
       get :show, format: 'json'
       hash_body = JSON.parse(response.body).with_indifferent_access
-      expect(hash_body.keys.include?('user')).to eq(true)
-      %w(id email first_name last_name company phone fax token).each { |key| expect(hash_body['user'].keys.include?(key)).to eq(true) }
+      expect(hash_body.keys.include?('user')).to eq true
+      %w(id email first_name last_name company phone fax token).each do |key|
+        expect(hash_body['user'].keys.include?(key)).to eq true
+      end
     end
   end
 end
