af4cddd2d1b3b6eb306bd2800ec8affc0d74d9d8
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-11 20:58:29 +0530

    Seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create Users
+User.create([
+                {email: 'pittohio@trantorinc.com', password: 'trantorpwd', first_name: 'Pittohio', last_name: 'User', company: 'Pittohio'},
+                {email: 'trantor@trantorinc.com', password: 'trantorpwd', first_name: 'Trantor', last_name: 'User', company: 'Trantor, Inc.'}
+            ])
 
-puser = User.create(email: 'pittohio@trantorinc.com', password: 'trantorpwd', first_name: 'Pittohio', last_name: 'User', company: 'Pittohio')
-User.create(email: 'trantor@trantorinc.com', password: 'trantorpwd', first_name: 'Trantor', last_name: 'User', company: 'Trantor, Inc.')
-
+# Create Roles Master
 Role::ALL.each { |role| Role.create(name: role) unless Role.find_by(name: role).present? }
