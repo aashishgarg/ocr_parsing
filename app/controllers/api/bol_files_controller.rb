28004b3ec036ef5ae57c2a265e4b1845db272cbe
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-11 21:37:08 +0530

    APi doc updated

--- app/controllers/api/bol_files_controller.rb
+++ app/controllers/api/bol_files_controller.rb
@@ -1,5 +1,6 @@
 class Api::BolFilesController < ApplicationController
   include Api::Concerns::BolFilesApipie
+  skip_authorization_check only: %i[show index]
 
   # Before Actions
   authorize_resource
