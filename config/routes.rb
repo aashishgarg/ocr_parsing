947917904dce2314bc599caa75f7f9e5a8a2a1b7
Himanshu Tiwari <himanshu.tiwari@trantorinc.com>
2019-01-14 16:40:57 +0530

    Added the todo comments.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   require "sidekiq/web"
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ## ToDo change the username and password to rails credentials.
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest('trantorinc')) &
       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest('trantorpwd'))
   end
