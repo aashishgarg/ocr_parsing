2fef5c2e7cf1b87bc569f89e7e04ddb33ac307e3
Himanshu Tiwari <himanshu.tiwari@trantorinc.com>
2019-01-16 13:27:10 +0530

    Change the status hash logic.

--- app/models/bol_file.rb
+++ app/models/bol_file.rb
@@ -51,11 +51,12 @@ class BolFile < ApplicationRecord
 
   def self.counts
     all = BolFile.all
+    status_hash = all.group_by(&:status).with_indifferent_access
     {
       total: count,
-      file_verified: all.group_by(&:status)[:ocr_done]&.count || 0,
-      waiting_for_approval: all.group_by(&:status)[:qa_approved]&.count || 0,
-      file_approved: all.group_by(&:status)[:released]&.count || 0
+      file_verified: status_hash[:ocr_done]&.count || 0,
+      waiting_for_approval: status_hash[:qa_approved]&.count || 0,
+      file_approved: status_hash[:released]&.count || 0
     }
   end
 
