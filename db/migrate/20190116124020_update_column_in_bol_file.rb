0a08fa26463521a6843e06c48fac83f1450146ed
Himanshu Tiwari <himanshu.tiwari@trantorinc.com>
2019-01-16 18:28:38 +0530

    Updated the indentation.

--- db/migrate/20190116124020_update_column_in_bol_file.rb
+++ db/migrate/20190116124020_update_column_in_bol_file.rb
@@ -1,5 +1,5 @@
 class UpdateColumnInBolFile < ActiveRecord::Migration[5.2]
-def change
+  def change
     add_column :bol_files, :extracted_at, :datetime
     remove_column :bol_files, :status_updated_at, :integer
   end
