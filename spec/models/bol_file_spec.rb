f2c61c965d4c59e6a388784bb97fe0ead0e8be6d
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-23 23:19:44 +0530

    Model specs updated

--- spec/models/bol_file_spec.rb
+++ spec/models/bol_file_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe BolFile, type: :model do
   before do
     @user = FactoryBot.create(:user)
   end
-  context 'validations:' do
+  context '#validations' do
+    it 'does not allow blank [status]' do
+      bol_file = FactoryBot.build(:bol_file, user: @user, name: nil, status: nil)
+      expect(bol_file.valid?).to eq(false)
+    end
+
     it 'with no name' do
       bol_file = FactoryBot.create(:bol_file, user: @user)
       expect(bol_file.valid?).to eq(true)
