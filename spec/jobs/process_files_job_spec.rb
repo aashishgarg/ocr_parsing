43fe58f6aa1f27309a21212079e3b0f3b5e79a87
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-23 20:44:02 +0530

    Job specs updated

--- spec/jobs/process_files_job_spec.rb
+++ spec/jobs/process_files_job_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 RSpec.describe ProcessFilesJob, type: :job do
   before do
     @user = FactoryBot.create(:user)
-    @user.add_role :admin
+    @bol_file = FactoryBot.create(:bol_file, user: @user)
   end
   it 'matches with enqueued job' do
     ActiveJob::Base.queue_adapter = :test
-    bol_file = FactoryBot.create(:bol_file, user: @user)
-    attach = bol_file.attachments.create(data: File.open("#{Rails.root}/spec/support/attachments/bol_files/448118"))
+
+    attach = @bol_file.attachments.create(data: File.open("#{Rails.root}/spec/support/attachments/bol_files/448118"))
     expect { ProcessFilesJob.perform_later(attach) }.to(have_enqueued_job(ProcessFilesJob))
     expect { ProcessFilesJob.perform_later(attach) }.to(have_enqueued_job.with { |attachment|
       expect(attachment).to eq(attach)
     })
+  end
+
+  it 'updates latest [updated_at] of all attachments in [extracted_at] of bolFile' do
+    %w[448118 448118.001 448118.002].each do |name|
+      ProcessFilesJob.perform_later(@bol_file.attachments.create(data: File.open("#{Rails.root}/spec/support/attachments/bol_files/#{name}")))
+    end
+    expect(@bol_file.extracted_at).to eq(@bol_file.attachments.collect(&:updated_at).max)
   end
 end
