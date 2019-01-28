68731976db2168a00a53af6337032454413a055f
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-01-28 10:57:35 +0530

    Orphaned routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   apipie
-  require "sidekiq/web"
+  require 'sidekiq/web'
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ## ToDo change the username and password to rails credentials.
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest('trantorinc')) &
       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest('trantorpwd'))
   end
-  mount Sidekiq::Web, at: "/sidekiq"
+  mount Sidekiq::Web, at: '/sidekiq'
 
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
@@ -16,10 +16,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resource :user, only: %i[show update]
     resources :roles, only: %i[index show destroy]
-    resources :bol_files do
-      resources :attachments, only: %i[update]
-    end
-
+    resources :bol_files
     get 'dashboard', to: redirect { |_, request| "/api/bol_files?dashboard=true&#{request.query_string}" }
   end
 
