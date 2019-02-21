623e57474fe6529401a5a4162e099d651c23132f
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-02-21 17:46:54 +0530

    Updated unit test cases for  handling new and existing scenarios.

--- spec/models/bol_file_spec.rb
+++ spec/models/bol_file_spec.rb
@@ -6,17 +6,22 @@ RSpec.describe BolFile, type: :model do
   end
   context '#validations' do
     it 'does not allow blank [status]' do
-      bol_file = FactoryBot.build(:bol_file, user: @user, name: nil, status: nil)
+      bol_file = FactoryBot.build(:bol_file, user: @user, status: nil)
       expect(bol_file.valid?).to eq(false)
     end
 
-    it 'with no name' do
-      bol_file = FactoryBot.create(:bol_file, user: @user)
+    it 'with [status]' do
+      bol_file = FactoryBot.build(:bol_file, user: @user)
       expect(bol_file.valid?).to eq(true)
     end
 
+    it 'with no name' do
+      bol_file = FactoryBot.build(:bol_file, user: @user, name: nil)
+      expect(bol_file.valid?).to eq(false)
+    end
+
     it 'with name' do
-      bol_file = FactoryBot.create(:bol_file, user: @user, name: nil)
+      bol_file = FactoryBot.create(:bol_file, user: @user)
       expect(bol_file.valid?).to eq(true)
     end
   end
@@ -24,7 +29,7 @@ RSpec.describe BolFile, type: :model do
   context '#Callback' do
     context 'when status = qa_approved' do
       before do
-        @bol_file = FactoryBot.create(:bol_file, user: @user, name: nil)
+        @bol_file = FactoryBot.create(:bol_file, user: @user, name: 'BolFile Name')
         @bol_file.update(status: 'qa_approved')
       end
       it 'sets qa_approved_at = DateTime' do
@@ -38,7 +43,7 @@ RSpec.describe BolFile, type: :model do
 
     context 'when status = released' do
       before do
-        @bol_file = FactoryBot.create(:bol_file, user: @user, name: nil)
+        @bol_file = FactoryBot.create(:bol_file, user: @user, name: 'BolFile Name')
         @bol_file.update(status: 'qa_approved')
         @bol_file.update(status: 'released')
       end
@@ -53,7 +58,7 @@ RSpec.describe BolFile, type: :model do
 
     context 'when status < qa_approved' do
       before do
-        @bol_file = FactoryBot.create(:bol_file, user: @user, name: nil)
+        @bol_file = FactoryBot.create(:bol_file, user: @user, name: 'BolFile Name')
         @bol_file.update(status: 'ocr_pending')
       end
       it 'sets qa_approved_at = nil' do
@@ -67,7 +72,7 @@ RSpec.describe BolFile, type: :model do
 
     context 'when status.between?(qa_approved and released)' do
       before do
-        @bol_file = FactoryBot.create(:bol_file, user: @user, name: nil)
+        @bol_file = FactoryBot.create(:bol_file, user: @user, name: 'BolFile Name')
         @bol_file.update(status: 'qa_approved')
         @bol_file.update(status: 'uat_rejected')
       end
